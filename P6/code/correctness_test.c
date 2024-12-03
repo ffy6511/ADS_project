602290f4a1d313542fe69852cc98c6f5088043e9
ffy6511 <1253876012@qq.com>
2024-12-03 15:52:06 +0800

    feat: partition method
    try to use partition in logM time

--- P6/code/correctness_test.c
+++ P6/code/correctness_test.c
@@ -1,23 +1,24 @@
-#include "solution.h"
+#include "adjustment.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 // 函数声明
+// BST functions
 TreeNode* createNode(Rectangle rect);
 TreeNode* insert(TreeNode* root, Rectangle rect);
 TreeNode* findLargestFit(TreeNode* root, int targetWidth);
 TreeNode* deleteNode(TreeNode* root, Rectangle rect);
 void cleanupBST(TreeNode* root);
-void initializeceiling_height(int width);
-void printceiling_height(int containerWidth);
-int findMaxWidth(int containerWidth, int* startIndex);
-void placeRectangle(int startIndex, int rectWidth, int rectHeight);
-int calculateHeight(int containerWidth);
-void adjustceiling_height(int startIndex, int width, int containerWidth);
-int ceiling_heightPacking(Rectangle *rectangles, int n, int containerWidth);
 
-// 全局变量
-int ceiling_height[MAX_WIDTH];
+// Partition and heap functions
+Partition* createPartition(int start_index, int width, int height);
+MinHeap* initialize_partition_heap(int containerWidth);
+Partition* extractMin(MinHeap *heap);
+void insertPartition(MinHeap *heap, Partition *part);
+void adjust_partition(MinHeap *heap, Partition *part);
+
+// Main packing algorithm
+int partitionPacking(Rectangle *rectangles, int n, int containerWidth);
 
 int main() {
     int containerWidth = 10; //max width of the container
@@ -28,7 +29,8 @@ int main() {
     };
     int n = sizeof(rectangles) / sizeof(rectangles[0]);
 
-    int finalHeight = ceiling_heightPacking(rectangles, n, containerWidth); //call the function to pack the rectangles
+   // Call the partition packing function
+    int finalHeight = partitionPacking(rectangles, n, containerWidth);
     printf("\nFinal container height: %d\n", finalHeight);
     return 0;
 }
